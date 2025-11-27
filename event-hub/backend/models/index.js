1b1dcd88f319156ba4a67ed7d21e8f3c012148fe
Heitor Borges <heitor.borges@ba.estudante.senai.br>
2025-11-27 09:38:11 -0300

    Fazer relacionamentos entre tabelas

---
+++ event-hub/backend/models/index.js
@@ -0,0 +1,34 @@
+const Organizador = require("./Organizadores")
+const Participante = require("./Participantes")
+const Evento = require("./Evento")
+const Ingresso = require("./Ingresso")
+
+Organizador.hasMany(Participante,{foreignKey: "createdBy", as: "participantesCriados"})
+Organizador.hasMany(Participante,{foreignKey: "updatedBy", as: "participantesAtualizados"})
+
+Participante.belongsTo(Organizador,{foreignKey: "createdBy", as: "criador", onDelete: "RESTRICT", onUpdate: "CASCADE"})
+Participante.belongsTo(Organizador,{foreignKey: "updatedBy", as: "atualizador", onDelete: "SET NULL", onUpdate: "CASCADE"})
+
+
+Organizador.hasMany(Evento,{foreignKey: "organizadorId", as: "eventos"})
+Evento.belongsTo(Organizador,{foreignKey: "organizadorId", as: "organizador"})
+
+
+Evento.hasMany(Ingresso,{foreignKey: "eventoId", as: "ingressos"})
+Ingresso.belongsTo(Evento,{foreignKey: "eventoId", as: "evento"})
+
+
+Participante.hasMany(Ingresso,{foreignKey: "participanteId", as: "compras"})
+Ingresso.belongsTo(Participante,{foreignKey: "participanteId", as: "participante"})
+
+
+Organizador.hasMany(Ingresso,{foreignKey: "vendedorId", as: "vendas"})
+
+Ingresso.belongsTo(Organizador,{foreignKey: "vendedorId", as: "vendedor"})
+
+module.exports ={
+    Organizador,
+    Participante,
+    Evento,
+    Ingresso
+}
